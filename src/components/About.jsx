4841733293b4e9cd5cb32b52d1aa54a6e13edcf6
tsegaywelu <tsegaygebrezgi56@gmail.com>
2024-05-10 07:23:21 +0300

    about updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,8 @@
 import React from "react";
-import { useContext, } from "react";
+import { useContext } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faUsers } from "@fortawesome/free-solid-svg-icons";
+
 import { LanguageContext } from "../components/contextprovider/Language";
 const About = () => {
   const { contextData } = useContext(LanguageContext);
@@ -7,94 +10,56 @@ const About = () => {
   //console.log("your language is" + contextData.Language);
   return (
     <div>
-      <h1 className=" text-center  font-bold text-green-900  animate-pulse justify-center text-3xl">
-               Golden business group
-      </h1>
-      <section className="">
+      <section className=" flex gap-3">
         <div className="py-3 bg-white">
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-            <div className="lg:text-center">
-              <h2 className="font-heading mb-4 bg-orange-100 text-orange-800 px-4 py-2 rounded-lg md:w-64 md:mx-auto text-xs font-semibold tracking-widest  uppercase title-font">
-                Why choose us?
-                {contextData.Language == "English" ? "News" : "ሓዱሽ ሓበሬታ"}
-              </h2>
-              <p className="font-heading mt-2 text-3xl leading-8 font-semibold tracking-tight text-gray-900 sm:text-4xl">
-                We know tech, we know finance. We are fintech experts.
+            <div className="">
+              <p className="font-heading mt-2 text-3xl leading-8 font-semibold tracking-tight text-green-900 sm:text-4xl lg:text-center">
+                {/* {contextData.Language == "English" ? "TDF assocition" : "ማሕበር ህንፀት ዘመናዊ ማእኸል ዕዳጋ(ሞል) ኣሉላ"} */}
+                ማሕበር ህንፀት ዘመናዊ ማእኸል ዕዳጋ(ሞል) ኣሉላ
               </p>
-              <p className="mt-4 max-w-2xl text-lg  text-black lg:mx-auto">
-                We know how to handle taxation for all the countried we operate
-                in. We care for our customers and help them manage cashflows.
-              </p>
-            </div>
+              <p className="mt-1  text-lg  text-black  ">
+                ፈላማይ ኣፍሪካዊ ጀነራል ራእሲ ኣሉላ ኣባ ነጋ ኣብ ከባቢ ተምቤን መነወ ተባሂሉ ኣብ ዝፅዋዕ ቦታ
+                ተወሊዱ ዝዓበየ ካብ <i className="text-blue-700">1827-1897 </i>ዓ/ም/ፈ
+                ዝነበረ ስሙይ ኣዋጋኣይን ተዋግኣይን ጀነራል እዩ። <br />
+                ራእሲ ኣሉላ ኣባ ነጋ ቅድሚ ዓወት ኲናት ዓድዋ ኣብ ኩፊት፣ ደጉዓሊ፣ሰሓጢት፣ ጉንደትን ጉራዕን ኣንፃር
+                ባዕዳውያን ወረርቲ ገጢሙ ብዓወት ዝዛዘመ ስሙይ ጀነራል እዩ ነይሩ።ራስ ኣሉላ ኣብ ነጋ ፈታዊ ዓዱ፣
+                ቆራፅ፣ ጅግና፣ በሊሕን መስተውዓልን ብዝኾነ ጥቕሚ ይኹን ምፍርራሕ ዓዱ ኣሕሊፉ ዘይህብ ኣብ ታሪኽ
+                ትግራኛ ተዛረብቲ ህዝብን፣ ህዝብታት ኢትዮጵያን ናይ ባዕሉ ዓብዪ ታሪኻዊ ዓሻራ ዘዕረፈን ኢትዮጵያ
+                ብባዕዳዊ ወረርቲ ከይተደፈረት ምስ ክብራ ክትቕፅል ዝገበረ ጅግና እዩ ነይሩ። <br />
+                <br />
+                በዚ ስሙይ ፈላማይ ኣፍሪካዊ ጀነራል ዝተሰየመት ማሕበርና ማሕበር{" "}
+                <i className="text-blue-700"> ህንፀት ዘመናዊ ማእኸል ዕዳጋ(ሞል)</i> ኣሉላ ኣብ{" "}
+                <i className="text-red-500">ጀኖሳይዳዊ ኲናት ትግራይ </i>ታሪኻዊ ኣበርክቶ ብዝፈፀሙ
+                ጀጋኑ ተጋደልቲ፣ ጀጋኑ ሓርበኛታትን ስድራ ስውኣትን ዝተመስረተት ማሕበር ኾይና፣ዘመናዊ ማእኸል ዕዳጋ
+                ሃኒፃ ኣብ ኢኮኖምያዊ ረብሓ ኣባላታን ትግራይ ናይ ባዕላ ኣወንታዊ ተራ ንኽትፃወት ትንቀሳቐስ ዘላ
+                ማሕበር እያ።
+                <br />
+                <div>
+                  <i className="text-blue-700 text-2xl block text-center">
+                    <FontAwesomeIcon icon="fa-solid fa-user" />
+                    <FontAwesomeIcon icon={faUser} /> ኣባላት እዛ ማሕባር
+                  </i>
 
-            <div className="mt-10">
-              <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
-                <div className="relative">
-                  <dt>
-                    <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-primary-500 text-white">
-                      <img src="https://www.svgrepo.com/show/503163/api-settings.svg" />
-                    </div>
-                    <p className="font-heading ml-16 text-lg leading-6 font-bold text-gray-700">
-                      Powerful API
-                    </p>
-                  </dt>
-                  <dd className="mt-2 ml-16 text-base  text-black">
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                    Maiores impedit perferendis suscipit eaque, iste dolor
-                    cupiditate blanditiis ratione.
-                  </dd>
+                  <div className="mx-auto " style={{ width: "40%" }}>
+                    <FontAwesomeIcon icon={faUser} /> ሕዚ ኣብ ሰራዊት ዘለዉ <br />
+                    <FontAwesomeIcon icon={faUser} /> ብኽበሪ ዝተሰናበቱ
+                    <br />
+                    <FontAwesomeIcon icon={faUser} /> ስድራ ስውኣት
+                    <br />
+                  </div>
                 </div>
-                <div className="relative">
-                  <dt>
-                    <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-primary-500 text-white">
-                      <img src="https://www.svgrepo.com/show/503138/webpack.svg" />
-                    </div>
-                    <p className="font-heading ml-16 text-lg leading-6 font-bold text-gray-700">
-                      Easy to integrate SDK
-                    </p>
-                  </dt>
-                  <dd className="mt-2 ml-16 text-base  text-black">
-                    {" "}
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                    Maiores impedit perferendis suscipit eaque, iste dolor
-                    cupiditate blanditiis ratione.
-                  </dd>
-                </div>
-                <div className="relative">
-                  <dt>
-                    <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-primary-500 text-white">
-                      <img src="https://www.svgrepo.com/show/511771/dashboard-671.svg" />
-                    </div>
-                    <p className="font-heading ml-16 text-lg leading-6 font-bold text-gray-700">
-                      Low Transaction Cost
-                    </p>
-                  </dt>
-                  <dd className="mt-2 ml-16 text-base  text-black">
-                    {" "}
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                    Maiores impedit perferendis suscipit eaque, iste dolor
-                    cupiditate blanditiis ratione.
-                  </dd>
-                </div>
-                <div className="relative">
-                  <dt>
-                    <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-primary-500 text-white">
-                      <img src="https://www.svgrepo.com/show/76267/free-commercial-label.svg" />
-                    </div>
-                    <p className="font-heading ml-16 text-lg leading-6 font-bold text-gray-700">
-                      Powerful Dashboard
-                    </p>
-                  </dt>
-                  <dd className="mt-2 ml-16 text-base  text-black">
-                    {" "}
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                    Maiores impedit perferendis suscipit eaque, iste dolor
-                    cupiditate blanditiis ratione.
-                  </dd>
-                </div>
-              </dl>
+                <br />
+                እዞም ትግራይ ንኸይትውረር ክቡር መስዋእቲ ዝኸፈሉ፣ ናይ ኣካል መጉዳእቲ ዝበፅሖም ሓርበኛታት፣ ሕዚ
+                እውን ምእንተ ትግራይ ኣብ ፈቐዶ በረኻ ዘለዉ ጀጋኑን ስድራ ጀጋኑን ብ ኢኮኖሚ ዓርሶም ክኽእሉ፣
+                ቁጠባዊ ዓቕሞም ከዕብዩን ስድርኦም ክልውጡን ከመሓደሩን ኣብ ጎኖም ኾይንና ኣጆኹም ክንብሎምን መንግስቲ
+                ይኹን ኩሉ ክፋል ሕብረተሰብ ናይ ባዕሉ ተራ ክፃወት ኣለዎ።
+              </p>
             </div>
           </div>
+        </div>
+        <div className=" lg:mt-11 h-full">
+          <img src="./images/tdf3.PNG" alt="" />
         </div>
       </section>
     </div>
